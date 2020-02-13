de6b68a64a48a95f7bc70db7c3965ce3e9195c5b
nasredine <nacer.cheniki@gmail.com>
2020-02-13 17:07:01 +0100

    add more dialogs.

--- app/src/main/java/sc/teach/fragmentsspeciaux/MainActivity.java
+++ app/src/main/java/sc/teach/fragmentsspeciaux/MainActivity.java
@@ -5,15 +5,21 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 import sc.teach.fragmentsspeciaux.dialogs.AlertDialogFragment;
+import sc.teach.fragmentsspeciaux.dialogs.DatePickerFragment;
 import sc.teach.fragmentsspeciaux.dialogs.SimpleDialogFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     Button frag_simple_btn;
     Button frag_alert_btn;
@@ -44,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ProgressDialog pd = new ProgressDialog(this);
+
+        pd.setTitle("Loading...");
+
+        pd.setMessage("Please wait.");
+
+        pd.setCancelable(false);
+
+        pd.show();
+
+        pd.dismiss();
+
 
     }
 
@@ -68,4 +86,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // attach to an onclick handler to show the date picker
+
+    public void showDatePickerDialog(View v) {
+
+        DatePickerFragment newFragment = new DatePickerFragment();
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+
+    }
+
+
+
+    // handle the date selected
+    //https://guides.codepath.com/android/using-dialogfragment#styling-custom-dialog
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        // store the values selected into a Calendar instance
+
+        final Calendar c = Calendar.getInstance();
+
+        c.set(Calendar.YEAR, year);
+
+        c.set(Calendar.MONTH, monthOfYear);
+
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+    }
 }
